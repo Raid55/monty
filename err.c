2229cf8a3038010c2799fa7529a90389d10fc020
Raid55 <boutboulnicholas@gmail.com>
2017-12-21 04:50:43 +0000

    error handler

---
+++ err.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+void _err(int errcode, char *info)
+{
+	(void) info;
+	
+	switch(errcode)
+	{
+		case NO_ARGS:
+			printf("USAGE: monty file");
+			break;
+		case NO_FILE:
+			printf("Error: Can't open file %s", info);
+			break;
+		case INVAL_LINE:
+			printf("L%d: unknown instruction %s", instance->line_number, info);
+			break;
+		case MALLOC_ERR:
+			printf("Error: malloc failed");
+			break;
+		default:
+			printf("U WUT M8???");
+	}
+	exit(EXIT_FAILURE);
+}
